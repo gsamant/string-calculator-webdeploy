3e279323fc7144fb7d62e67eb91bce363b681061
Gandhali <gsamant@github.com>
2022-08-25 09:19:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const http = require('http')
+const qs = require('querystring')
+const calculator = require('./calculator')
+ 
+const server = http.createServer(function(request, response) {
+  console.dir(request.param)
+ 
+  if (request.method == 'POST') {
+    console.log('POST')
+    var body = ''
+    request.on('data', function(data) {
+      body += data
+    })
+ 
+    request.on('end', function() {
+      const post = qs.parse(body)
+      const numbers = post.numbers
+      const result = calculator.add(numbers)
+      response.writeHead(200, {'Content-Type': 'text/html'})
+      response.end(`<input type="text" name="result" value= Result:`+ result+` readonly>`)
+    })
+  } else {
+    var html = `
+            <html>
+                <body>
+                    <form method="post" action="`+url+`">Numbers: 
+                        <input type="text" name="numbers" />
+                        <input type="submit" name="add" value="Add" />
+                    </form>
+                </body>
+            </html>`
+    response.writeHead(200, {'Content-Type': 'text/html'})
+    response.end(html)
+  }
+})
+ 
+const port = process.env.PORT || 3000;
+const url = process.env.URL || "http://localhost:3000";
+server.listen(port);
+console.log(`Listening at http://localhost:${port}`)
